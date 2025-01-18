fca28e08ada836a42f68ea818e5a89130b01deaa
Parry-Zhuo <165851545+Parry-Zhuo@users.noreply.github.com>
2025-01-17 16:32:07 -0800

    Has json file

--- PyPlot.py
+++ PyPlot.py
@@ -1,31 +1,32 @@
 class DataPlotter:
     """
-    @brief A class for creating and managing plots using Matplotlib's PyPlot.
-
-    @Params
-    attributes - this is signified, 
+    @brief A class inputs a dictionary. Example shown in example.json
+    
+    @Params json file.
 
     @Functions:
-
-    Change parameters,
+    Filter by attribute
     Print out list
 
-    @ Questions or observations:
-
-    The data required by the GUI will be returned, no GUI will be implemented in this class
-    Input will not interact with GUI either, it expects DICTIONARY.      
-    TBH, is class even needed for such a task?
-    
     Example usage:
     @code
         plotter = DataPlotter()
-        plotter.line_plot([1, 2, 3], [4, 5, 6], title="Example Plot", xlabel="Time", ylabel="Value")
+        plotter.sort("Attribute_Name_1")
+        plotter.line_plot()
     @endcode
 
     @note This class requires Matplotlib to be installed.
     """
-    def __init__(self, attributes):
+    def __init__(self):
         self.potato = 1
+        self.graphdata = [];
+
+    #@params 
+    def DataPlotter_insertData(self,graphdata):
+        self.graphdata = graphdata;
+    
+    def DataPlotter_sort(self):
+        i=1;
 
 if __name__ == "__main__":
     print("sandwhich")
