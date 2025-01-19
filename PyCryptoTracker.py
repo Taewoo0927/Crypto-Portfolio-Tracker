d3a4fa48cb783982bee219cd266c099561c908e1
Parry-Zhuo <165851545+Parry-Zhuo@users.noreply.github.com>
2025-01-19 13:41:58 -0800

    Attempt to merge first PYPLOT

---
+++ PyCryptoTracker.py
@@ -0,0 +1,20 @@
+# Dependencies:
+# python -m pip install requests
+
+import requests
+
+url = "https://api.coingecko.com/api/v3/ping"
+
+# For professional subscriptions, switch "x-cg-pro-api-key" with "x-cg-demo-api-key"
+headers = {
+    "accept": "application/json",
+    "x-cg-demo-api-key": "apikey"
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error: {response.status_code}, {response.text}")
+
